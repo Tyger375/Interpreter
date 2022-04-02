ba40676ea0c45ca00c275d9475b1cd15177f4f30
Tyger375 <edoardo.takanen@gmail.com>
2022-04-02 19:43:46 +0200

    test

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "./Interpreter/Interpreter.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    Interpreter interpreter;
+    if (argc == 2)
+    {
+        interpreter.start(argv[1]);
+    }
+    else
+    {
+        string line;
+        do
+        {
+            cout << ">>> ";
+            getline(cin, line);
+            interpreter.Line(line);
+            //interpreter.debugVariables();
+        }
+        while (line != "exit");
+        interpreter.debugVariables();
+        //cout << "Real-time interpreter is not available. It'll be added soon" << endl;
+    }
+    return 0;
+}
